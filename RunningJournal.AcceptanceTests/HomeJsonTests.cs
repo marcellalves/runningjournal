85255f6f3170cc02ffa821bc6e6ff40b481051e2
Marcell Nascimento Alves <marcell.alves@squadra.com.br>
2018-05-29 09:50:37 -0300

    Walking skeleton web api

---
+++ RunningJournal.AcceptanceTests/HomeJsonTests.cs
@@ -0,0 +1,30 @@
+﻿using RunningJournal.Api;
+using System;
+using System.Net.Http;
+using System.Web.Http.SelfHost;
+using Xunit;
+
+namespace RunningJournal.AcceptanceTests
+{
+    public class HomeJsonTests
+    {
+        [Fact]
+        public void GetResponseReturnCorrectStatusCode()
+        {
+            var baseAddress = new Uri("http://localhost:9876");
+            var config = new HttpSelfHostConfiguration(baseAddress);
+            new Bootstrap().Configure(config);
+            var server = new HttpSelfHostServer(config);
+            using (var client = new HttpClient(server))
+            {
+                client.BaseAddress = baseAddress;
+
+                var response = client.GetAsync("").Result;
+
+                Assert.True(
+                    response.IsSuccessStatusCode,
+                    "Actual status code: " + response.StatusCode);
+            }
+        }
+    }
+}
---
+++ RunningJournal.Api/JournalController.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace RunningJournal.Api
+{
+    public class JournalController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return this.Request.CreateResponse();
+        }
+    }
+}
